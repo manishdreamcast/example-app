5536545fb0c66abf9f72424f08f7560775a898f8
manishdreamcast <manish@dreamcast.co>
2024-03-11 17:49:00 +0530

    seconed add

--- app/Http/Controllers/Web/V1/AuthController.php
+++ app/Http/Controllers/Web/V1/AuthController.php
@@ -11,6 +11,27 @@ class AuthController extends Controller
     {
         try{
 
+            $rules = [
+                'email' => 'required|email',
+                'first_name' => 'required|string|max:50', // Limiting first name to 50 characters
+                'last_name' => 'required|string|max:50', // Limiting last name to 50 characters
+            ];
+            
+            $messages = [
+                'email.required' => 'Please provide your email address.',
+                'email.email' => 'Please provide a valid email address.',
+                'first_name.required' => 'Please enter your first name.',
+                'last_name.required' => 'Please enter your last name.',
+                'first_name.max' => 'The first name should not exceed 50 characters.',
+                'last_name.max' => 'The last name should not exceed 50 characters.',
+            ];
+            
+            $validator = Validator::make($request->all(), $rules, $messages);
+
+            if ($validator->fails()) {
+                return jsonResponse::error($validator->errors()->first(),['validationError'=>$validator->errors()]);
+            }
+
         }catch(Exception $e)
         {
             
