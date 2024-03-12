41b0916793da05e356625c6441c545dfc22795b1
manishdreamcast <manish@dreamcast.co>
2024-03-12 09:35:24 +0530

    dsf

--- app/Http/Controllers/Web/V1/AuthController.php
+++ app/Http/Controllers/Web/V1/AuthController.php
@@ -13,8 +13,8 @@ class AuthController extends Controller
 
             $rules = [
                 'email' => 'required|email',
-                'first_name' => 'required|string|max:50', // Limiting first name to 50 characters
-                'last_name' => 'required|string|max:50', // Limiting last name to 50 characters
+                'first_name' => 'required|string|max:50|regex:/^[a-zA-Z]+$/u', // Limiting first name to 50 characters and allowing only alphabetic characters
+                'last_name' => 'required|string|max:50|regex:/^[a-zA-Z]+$/u', // Limiting last name to 50 characters and allowing only alphabetic characters
             ];
             
             $messages = [
@@ -24,17 +24,21 @@ class AuthController extends Controller
                 'last_name.required' => 'Please enter your last name.',
                 'first_name.max' => 'The first name should not exceed 50 characters.',
                 'last_name.max' => 'The last name should not exceed 50 characters.',
+                'first_name.regex' => 'The first name should contain only alphabetic characters.',
+                'last_name.regex' => 'The last name should contain only alphabetic characters.',
             ];
             
             $validator = Validator::make($request->all(), $rules, $messages);
-
+            
             if ($validator->fails()) {
-                return jsonResponse::error($validator->errors()->first(),['validationError'=>$validator->errors()]);
+                return jsonResponse::error($validator->errors()->first(), ['validationError' => $validator->errors()]);
             }
+
 
         }catch(Exception $e)
         {
-            
+            Log::error($e->getMessage());
+            return jsonResponse::error(getenv('APP_DEBUG') ? $e->getMessage():'Something Went Wrong!');
         }
     }
 }
